4e5973b77b62ab33b2f26adfc698a56a58b0015b
ramchandra06032004 <rajumulik51@gmail.com>
2025-05-04 16:39:19 +0530

    add multiple regression analysis with Iris dataset

--- multiple_regression.r
+++ multiple_regression.r
@@ -1,1 +1,18 @@
 library(readr)
+library(nnet)
+df=read_csv("Iris.csv")
+n=nrow(df)
+n
+
+randomeIndex=sample(1:n,0.75*n)
+randomeIndex
+trainData=df[randomeIndex,]
+View(trainData)
+testData=df[-randomeIndex,]
+View(testData)
+model<-multinom(Species~SepalLengthCm+SepalWidthCm+PetalLengthCm+PetalWidthCm, data=trainData)
+summary(model)
+pridictions<-predict(model,testData)
+accuracy<-mean(pridictions==testData$Species)
+accuracy
+
