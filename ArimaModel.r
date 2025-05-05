95133f4e815900179ef82473970a9ec3cccaba82
ramchandra06032004 <rajumulik51@gmail.com>
2025-05-05 18:13:28 +0530

    Add initial data analysis and preprocessing for heart disease dataset
    - Imported necessary libraries: pandas and numpy
    - Loaded heart disease dataset from CSV file
    - Displayed dataset shape and information
    - Checked for missing values in the dataset
    - Analyzed zero values in the dataset
    - Split dataset into features and target variable
    - Created training and testing sets for model training

--- ArimaModel.r
+++
@@ -1,9 +0,0 @@
-library(tseries)
-time_data=ts(rnorm(100,mean = 50,sd = 10),start = c(2025,1),frequency = 12)
-library(forecast)
-model=arima(time_data)
-summary(model)
-forcasted=forecast(model,12)
-plot(forcasted)
-forcasted
-accuracy(forcasted)
